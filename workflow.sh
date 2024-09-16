a18ac8e18507475a0cc80c32c4dd4e90b8db2cd6
Franz Schug <67633718+franzschug@users.noreply.github.com>
2024-09-16 13:07:21 -0500

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -16,14 +16,20 @@ STACK=FALSE
 FITAR=FALSE
 
 ## Estimate  spatial autocorrelation parameters, range and nugget
-EST_AUTOCORR_PRM=TRUE
+EST_AUTOCORR_PRM=FALSE
 
+## Prepare, tile independent variables
+PREPINDEP=TRUE
 
-## Merge pixel-wise trend with explanatory variables
-MERGEEXPL=FALSE
+## Merge pixel-wise trend with independent variables
+MERGEINDEP=FALSE
 
-## Merge pixel-wise trend with explanatory variables
+## Generate partition matrix for complete dataset
+PARTITIONMATRIX=FALSE
+
+## Split data into randomized partitions
 PARTITION=FALSE
+
 
 SPLITGLS=FALSE
 
@@ -32,8 +38,6 @@ SPLITANALYSIS=FALSE
 #partition data!
 #run split parallel gls
 # split analysis
-
-POSTPROCESS=FALSE
 
 
 if [ $MERGEHII == TRUE ] ; then
@@ -95,7 +99,6 @@ if [ $FITAR == TRUE ] ; then
 	done
 fi
 
-
 if [ $EST_AUTOCORR_PRM == TRUE ] ; then
 	# Randomly sample n tiles, n pixels per tile, and n iterations
 	sample_tiles=400  	## Number of tiles to be randomly chosen for analysis
@@ -112,38 +115,110 @@ if [ $EST_AUTOCORR_PRM == TRUE ] ; then
 	dirstack=$WORKDIR'/011_data/hii/v1/00_stack/'
 	
 	# Estiamte range parameter, shuf - shuffle, tail - last elements
-	#ls $dir | grep .tif | shuf | tail -$sample_tiles | parallel -j 10 Rscript $WORKDIR'/090_scripts/parts_fitcor.R' $dir $n_per_tile $iterations $outFileSPCORS $outDirSPCORS {}
+	ls $dir | grep .tif | shuf | tail -$sample_tiles | parallel -j 10 Rscript $WORKDIR'/090_scripts/parts_fitcor.R' $dir $n_per_tile $iterations $outFileSPCORS $outDirSPCORS {}
 	
 	# Estimate nugget parameter
-	nr_samples=3000
+	nr_samples=5000
 	
 	ls $dir | grep .tif | shuf |tail -$sample_tiles | parallel -j 10 Rscript $WORKDIR'/090_scripts/parts_estimate_nugget.R' $dir $dirstack $nr_samples $WORKDIR'/011_data/parts/alls_spcors.txt' {}
-	#Rscript $WORKDIR'/090_scripts/parts_estimate_nugget.R' $WORKDIR'/011_data/hii/v1/ar_all_4/' $WORKDIR'/011_data/hii/v1/00_stack/' $WORKDIR'/011_data/parts/alls_spcors.txt' hii_-95_40.tif
 	
-	
-	#python3 $WORKDIR'/090_scripts/plots/00_distribution_range.py'
-	#python3 $WORKDIR'/090_scripts/plots/00_distribution_nugget.py'
+	python3 $WORKDIR'/090_scripts/plots/00_distribution_range.py'
+	python3 $WORKDIR'/090_scripts/plots/00_distribution_nugget.py'
 fi
 
-if [ $MERGEEXPL == TRUE ] ; then
+#indep_dir=( '011_data/countries' '011_data/dhi' '011_data/wdpa' '011_data/ecoregions' '011_data/species_richness' '011_data/species_richness' '011_data/species_richness' '011_data/species_richness' '011_data/wui' '011_data/gdp' '011_data/gdp' '011_data/gdp' '011_data/gdp' ) 
 
+indep_dir=( '011_data/wui' '011_data/gdp' '011_data/gdp' '011_data/gdp' '011_data/gdp' ) 
+
+#indep_name=( 'countries' 'cumDHI' 'wdpa_prox' 'ecoregions' 'sumTotalMammals' 'sumTotalBirds' 'sumTotalAmphibians' 'sumTotalReptiles' 'global_wui' 'gdp_1990' 'gdp_2015' 'gdp_change' 'gdp_rel_change' ) 
+
+indep_name=( 'global_wui' 'gdp_1990' 'gdp_2015' 'gdp_change' 'gdp_rel_change' ) 
+if [ $PREPINDEP == TRUE ] ; then
+	for (( COUNTERX=-180; COUNTERX<=175; COUNTERX+=5 )); do
+		for (( COUNTERY=90; COUNTERY>=-85; COUNTERY-=5 )); do
+			echo $COUNTERX
+			echo $COUNTERY
+			
+			xend=$(($COUNTERX+5))
+			yend=$(($COUNTERY-5))
+			
+			# Check if directories and names have different length
+			if [ ! "${#indep_dir[@]}" -eq "${#indep_name[@]}" ]; then
+			  echo "List of directories and list of names have different length!"
+			  break
+			fi
+
+			for p in $(seq 1 ${#indep_dir[@]}); do
+				i=$(($p - 1))
+				echo ${indep_dir[$i]}
+				echo ${indep_name[$i]}
+				echo $WORKDIR'/'${indep_dir[$i]}'/'${indep_name[$i]}'.tif'
+				echo $WORKDIR'/'${indep_dir[$i]}'/tiles/'${indep_name[$i]}'_'$COUNTERX'_'$COUNTERY.vrt
+				gdal_translate -projwin $COUNTERX $COUNTERY $xend $yend -of VRT -ot Int32 -a_nodata -999 $WORKDIR'/'${indep_dir[$i]}'/'${indep_name[$i]}'.tif' $WORKDIR'/'${indep_dir[$i]}'/tiles/'${indep_name[$i]}'_'$COUNTERX'_'$COUNTERY.vrt
+			done
+		done
+	done
+	
+	for p in $(seq 1 ${#indep_dir[@]}); do
+		i=$(($p - 1))
+		gdalwarp $WORKDIR'/'${indep_dir[$i]}'/tiles/'${indep_name[$i]}'_-95_40.vrt' $WORKDIR'/'${indep_dir[$i]}'/tiles/'${indep_name[$i]}'_-95_40.tif'
+	done
+fi
+
+
+if [ $MERGEINDEP == TRUE ] ; then
+	for (( COUNTERX=-180; COUNTERX<=175; COUNTERX+=5 )); do
+		for (( COUNTERY=90; COUNTERY>=-85; COUNTERY-=5 )); do
+			echo $COUNTERX
+			echo $COUNTERY
+			temp=("${indep_var[@]}")
+			temp=( "${temp[@]/%/"$COUNTERX"_"$COUNTERY".vrt}" )
+			temp=( "${temp[@]/#/"$WORKDIR"}" )
+			#printf '%s ' "${temp[@]}"
+			printf "${temp[*]}"
+
+			gdal_merge.py -o $WORKDIR'/data/FS_human_footprint/011_data/hii/v1/merged_ar_ind/hii_'$COUNTERX'_'$COUNTERY'_ind.tif' -co "COMPRESS=LZW" -separate "${temp[@]}"
+			
+			#'/data/FS_human_footprint/011_data/hii/v1/ar_coeff_pv/tif/AF_temp/hii_'$COUNTERX'_'$COUNTERY'.tif' '/data/FS_human_footprint/011_data/dhi/tiles_xy_fix/cumDHI_'$COUNTERX'_'$COUNTERY'.vrt' '/data/FS_human_footprint/011_data/wdpa/tiles_xy_fix/wdpa_prox_'$COUNTERX'_'$COUNTERY'.vrt' '/data/FS_human_footprint/011_data/ecoregions/tiles_xy_fix/ecoregions_'$COUNTERX'_'$COUNTERY'.vrt' '/data/FS_human_footprint/011_data/gdp/tiles_xy_fix/gdp_2015_'$COUNTERX'_'$COUNTERY'.tif' '/data/FS_human_footprint/011_data/species_richness/tiles/sprich_300_'$COUNTERX'_'$COUNTERY'.tif'
+		done
+	done
+fi
+
+if [ $PARTITIONMATRIX == TRUE ] ; then
+	global_vrt_path=$WORKDIR'011_data/hii/v1/merged_ar_ind/'*'.vrt'
+	pm_path=$WORKDIR'011_data/parts/pm/global_partition.rds'
+	partition_size=2000
+	
+	gdalbuildvrt $WORKDIR'011_data/hii/v1/merged_hii_ind.vrt' $global_vrt_path
+
+	Rscript $WORKDIR'/090_scripts/parts_generate_pm.R' $global_vrt_path $pm_path
 fi
 
 if [ $PARTITION == TRUE ] ; then
-
+	data_path=
+	$WORKDIR'011_data/parts/pm/global_partition.rds'
+	pm_path=$WORKDIR'011_data/parts/pm/global_partition.rds'
+	sub_pm_outdir=$WORKDIR'011_data/parts/pm/'
+	
+	Rscript $WORKDIR'/090_scripts/parts_split_partitions.R' $data_path $pm_path $sub_pm_outdir
 fi
 
 if [ $SPLITGLS == TRUE ] ; then
-
+perform vif and warn!
+print if vif high
+	confirmVIF=TRUE # If independent variables highly correlated accoding to VIF, stop to confirm
+	
 fi
 
 if [ $SPLITANALYSIS == TRUE ] ; then
 
 fi
-#MERGEEXPL 
+
+
+	
+		
 #- output correlation matrix, variance inflation factor test. then select!
 # write vif new - with subset of the data
-
 
 
 
