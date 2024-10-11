48172c13cb213a530779b37d57e8790c14902195
Franz Schug <67633718+franzschug@users.noreply.github.com>
2024-10-11 10:45:42 -0500

    Update workflow_parallel_parts.sh

--- workflow_parallel_parts.sh
+++ workflow_parallel_parts.sh
@@ -7,7 +7,7 @@ cd "$parent_path"
 ### arg1: path to input dataset
 ### arg2: path to save partition matrix (input if existing, output if not existing), needs to match input dataset!
 ### arg3: output dir to save partition matrix
-Rscript ./090_scripts/parts_split_partitions.R  ./011_data/hii/v1/ar_coeff_pv/csv/hii_global_reduced_subset_5million.csv ./011_data/parts/pm/pm_global_300m_5million.rds ./011_data/parts/pm/gls_300m_subpm/
+#Rscript ./090_scripts/parts_split_partitions.R  ./011_data/hii/v1/ar_coeff_pv/csv/hii_global_reduced_subset_5million.csv ./011_data/parts/pm/pm_global_300m_5million.rds ./011_data/parts/pm/gls_300m_subpm/
 
 ### collect all part files
 fileArr=()
@@ -22,6 +22,6 @@ for file in ./011_data/parts/pm/gls_300m_subpm/rds/*
 ### arg3: path to nugget estimates
 Rscript ./090_scripts/parts_split_gls.R ./011_data/parts/pm/gls_300m_subpm/rds/sub_partition_data_1.rds ./011_data/parts/alls_spcors.txt ./011_data/parts/alls_nuggets.txt
 ### FIX FIX TODO TODO parallel -j 30 Rscript ./090_scripts/parts_split_gls.R ::: ${fileArr[@]} ./011_data/parts/alls_spcors.txt ./011_data/parts/alls_nuggets.txt
-
+  
 ### merge results from all parts
-Rscript ./090_scripts/parts_split_analysis.R
+#Rscript ./090_scripts/parts_split_analysis.R
