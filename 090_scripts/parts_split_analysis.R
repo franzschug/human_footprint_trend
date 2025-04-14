76d6cba356d0c479608fb5a13f16052e7d1b34ec
Franz Schug <67633718+franzschug@users.noreply.github.com>
2025-04-14 11:16:56 -0500

    Update parts_split_analysis.R

--- 090_scripts/parts_split_analysis.R
+++ 090_scripts/parts_split_analysis.R
@@ -23,13 +23,17 @@ if (length(args)!=5) {
 START_TIME = Sys.time()
 
 outPath = paste0(outPath, '_', subset_gls, '_', subset_pairs, '.rds')
+print(gls_inDir)
+print(cross_inDir)
+print(outPath)
 
+print(subset_gls)
 ## load all gls files or subset
 gls_list = list.files(path = gls_inDir, pattern = "\\.rds$", full.names = TRUE)
-
 if (subset_gls != 0) {
 	gls_list = sample(gls_list, subset_gls)
 }
+
 
 ## load cross pairs
 cross_list = list.files(path = cross_inDir, pattern = "\\.rds$", full.names = TRUE)
@@ -37,29 +41,28 @@ cross_list = list.files(path = cross_inDir, pattern = "\\.rds$", full.names = TR
 if (subset_pairs != 0) {
 	cross_list = sample(cross_list, subset_pairs)
 }
-
 npairs = length(cross_list)
 
 ## extract dimensions
 dimGLS = readRDS(gls_list[1], refhook = NULL)
 p   = ncol(dimGLS$xx)
 p0  = ncol(dimGLS$xx0)
-
 coefs = SEs = tstats = tpvals = matrix(NA, nrow = length(gls_list), ncol = p, dimnames = list(NULL, names(dimGLS$coeff)))
 nuggets = LLs = SSEs = MSEs = MSRs = Fstats = Fpvals = rep(NA, times = length(gls_list))
 covar_coefs = array(NA, dim = c(p, p, length(gls_list)), dimnames = list(names(dimGLS$coeff), names(dimGLS$coeff), NULL))
-
 
 glsCounter = 0
 for (gls in gls_list) {
 	print(glsCounter)
 	glsCounter = glsCounter + 1
 	glsV = readRDS(gls, refhook = NULL)
+	print(length(glsV$coeff))
 	coefs[glsCounter,] = glsV$coeff
 	Fstats[glsCounter] = glsV$Fstat
 	covar_coefs[ , , glsCounter] = glsV$covar_coef
 }  
 
+print(colMeans(coefs))
 
 
 ## cross stats
